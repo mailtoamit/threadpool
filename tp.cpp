4811f30dbc4978ce36625bd82ef80df6f63a6114
Amit Mittal <amitmittal@cloudzilla.com>
2018-09-11 01:24:24 +0530

    Use lock for queuing.
    Create nested threads for test.

--- tp.cpp
+++ tp.cpp
@@ -9,24 +9,30 @@ class ThreadPool;
 
 class Work {
 public:
-    unsigned virtual runL() = 0;
+    unsigned virtual runL(void *arg) = 0;
     virtual ~Work() {};
+    void *ct;
 };
 
 class ThreadPool {
 public:
     static void* worker(void *arg);
     static ThreadPool* getInstance() {
-        static ThreadPool *tp = new ThreadPool();
-        return tp;
+        static ThreadPool tp;
+        return &tp;
     };
-    ~ThreadPool() { };
+    ~ThreadPool() {
+        void *ret;
+        for(int i = 0;i<SIZE;i++) {
+            pthread_join(th[i],&ret);
+        }
+    };
     
     static void queWork(Work *w) {
-      //pthread_mutex_lock(&mutex); // ?? When it can be needed 
       ThreadPool *t = getInstance();
+      pthread_mutex_lock(&t->qLock);
       t->wq.push(w);
-      //pthread_mutex_unlock(&mutex);
+      pthread_mutex_unlock(&t->qLock);
       
       sem_post(&t->s);
     };
@@ -34,10 +40,10 @@ private:
     ThreadPool() {
         init_threadpool();
     };
-
     void init_threadpool() {
         sem_init(&s,0,0);
-        mutex = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
+        qLock = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
+        dqLock = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
         
         for(int i = 0;i<SIZE;i++) {
             pthread_create(&th[i],NULL,&ThreadPool::worker,this);
@@ -46,46 +52,58 @@ private:
     
     pthread_t th[SIZE];
     sem_t s;
-    pthread_mutex_t mutex;
+    pthread_mutex_t qLock, dqLock;
     queue<Work*> wq;
+    static int thr_cnt;
 };
 
+int ThreadPool::thr_cnt = 0;
 void* ThreadPool::worker(void *arg) {
    ThreadPool *tp = (ThreadPool*)arg;
    while(1) {
       sem_wait(&(tp->s));
       Work *w=0;
 
-      pthread_mutex_lock(&tp->mutex);
+      pthread_mutex_lock(&tp->dqLock);
       if(!tp->wq.empty()) {   
          w = tp->wq.front();
          tp->wq.pop();
       }
-      pthread_mutex_unlock(&tp->mutex);
+      pthread_mutex_unlock(&tp->dqLock);
          
       if (w) 
-         w->runL();
+         w->runL(w->ct);
    }
    return 0;
 };
 
+
+
+///////Driver Code
 class MyWork: public Work {
 public:
-    MyWork(int i):tid(i) {}
+    MyWork(void *arg) {
+        ct = arg;
+    }
     ~MyWork() {}
-    unsigned runL() {
-        cout  << tid << " th" << endl;
-        usleep(tid*1000000);
+    unsigned runL(void *arg) {
+        if(arg) {
+            Work *w = new MyWork((void *)((long)arg/1000));
+            ThreadPool::queWork(w);
+            usleep((long)arg);
+        }
+        else
+            usleep(1000000);
+        cout  << this << " \n";
         return 0;
     }
-private:
-    int tid;
 };
 
-int main() {
+int main()
+{
     Work *wo[10];
     for (int i = 0;i<10; i++) {
-        wo[i] = new MyWork(i);
+        wo[i] = new MyWork((void*)50000);
         ThreadPool::queWork(wo[i]);
     }
     usleep(50000000);
